72849555e1aac6d72e19b7dcb282ddfeb6ef4ad0
Yoan Sallami <ysallami@laas.fr>
2018-05-17 18:37:03 +0200

    fix publishing of perspectives

--- scripts/perspective_filter_node.py
+++ scripts/perspective_filter_node.py
@@ -6,17 +6,19 @@ import underworlds
 import rospy
 import sys
 from underworlds import *
-from underworlds.helpers.geometry import _get_parent_chain
-from std_msgs.msg import String
-from perspective_filter.msg import Perspective, PerspectiveArray
+from underworlds.helpers.transformations import *
+from underworlds.helpers.geometry import *
+from std_msgs.msg import String, Header
+from geometry_msgs.msg import Pose, PoseStamped
+from perspectives_msgs.msg import Perspective, PerspectiveArray, PerspectiveArrayStamped, Agent, Object
 from perspective_filter.srv import AddAgent, RemoveAgent
 from underworlds.tools.visibility import VisibilityMonitor
 from underworlds.types import *
 
-EPSILON = 0.001
+EPSILON = 0.025
 
 class PerspectiveFilter(object):
-    def __init__(self, ctx, source_world):
+    def __init__(self, ctx, source_world, reference_frame):
         """
         Initialize the beliefs filter
         :param ctx: The underworlds context
@@ -27,6 +29,8 @@ class PerspectiveFilter(object):
         self.ctx = ctx
         # The source world name to monitor
         self.source_world_name = source_world
+
+        self.reference_frame = reference_frame
         # The source world to monitor
         self.source = self.ctx.worlds[source_world]
         # The beliefs worlds map stored by agent name
@@ -45,6 +49,7 @@ class PerspectiveFilter(object):
         self.agents = []
 
         self.visible_nodes = {}
+        self.previously_visible_nodes = {}
 
         self.current_situations_map = {}
 
@@ -57,13 +62,13 @@ class PerspectiveFilter(object):
 
         # The ROS publishers
         self.ros_publishers = {
-            "perspectives": rospy.Publisher("perspective_filter/agent_perspectives", PerspectiveArray, queue_size=10)}
+            "perspectives": rospy.Publisher("perspective_filter/agent_perspectives", PerspectiveArrayStamped, queue_size=10)}
         # The ROS services
         self.ros_services = {
             "add_agent": rospy.Service("perspective_filter/add_agent", AddAgent, self.handle_add_agent),
             "remove_agent": rospy.Service("perspective_filter/remove_agent", RemoveAgent, self.handle_remove_agent)}
 
-        self.log_pub = {"situation_log": rospy.Publisher("perspective_filter/log", String, queue_size=5)}
+        self.log_pub = {"isVisibleBy": rospy.Publisher("predicates_log/visibleby", String, queue_size=5)}
 
     def get_all_cameras(self):
         cameras = {}
@@ -100,14 +105,13 @@ class PerspectiveFilter(object):
         new_nodes_to_update = {}
         nodes_to_update = {}
 
-        self.cameras = self.get_all_cameras()
-
         for agent_id in self.cameras.keys():
-            #if agent_id in self.tom_enabled_ids:
             nodes_to_update[agent_id] = []
             new_nodes_to_update[agent_id] = []
             if agent_id not in self.beliefs:
-                self.beliefs[agent_id] = self.ctx.worlds[self.cameras[agent_id].name+"_beliefs"]
+                world_name = self.cameras[agent_id].name.replace("-","_")+"_beliefs"
+                rospy.logdebug("[perspective_filter] create new world <%s>" % str(world_name))
+                self.beliefs[agent_id] = self.ctx.worlds[world_name]
                 self.node_mapping[agent_id] = {}
 
         dq = deque()
@@ -117,11 +121,10 @@ class PerspectiveFilter(object):
             node = dq.pop()
             if node.id != self.source.scene.rootnode.id:
                 # Process start here
-
                 if node.id in self.cameras.keys():  # if the node is the agent POV
                     nodes_to_update[node.id].append(node)  # we add it to his belief
 
-                if node.parent in self.cameras.keys():  # if the node is part of an agent
+                if node.parent in self.cameras.keys() and node.type == MESH:  # if the node is part of an agent
                     nodes_to_update[node.parent].append(node)  # we add it to his belief
 
                 for agent_id, visible_nodes in self.visible_nodes.items():  # then we add the visible nodes
@@ -142,6 +145,10 @@ class PerspectiveFilter(object):
                         if new_node.id in self.nodes_transform:
                             if not numpy.allclose(self.nodes_transform[new_node.id], new_node.transformation):
                                 new_nodes_to_update[agent_id].append(new_node)
+                                self.nodes_transform[new_node.id] = new_node.transformation
+                        else:
+                            self.nodes_transform[new_node.id] = new_node.transformation
+                            new_nodes_to_update[agent_id].append(new_node)
                     else:
                         self.node_mapping[agent_id][node.id] = new_node.id
                         new_nodes_to_update[agent_id].append(new_node)
@@ -151,49 +158,115 @@ class PerspectiveFilter(object):
             for node in nodes:
                 node.parent = self.node_mapping[agent_id][node.parent] if node.parent in self.node_mapping[agent_id] \
                     else self.beliefs[agent_id].scene.rootnode.id
-                self.nodes_transform[new_node.id] = new_node.transformation
             if nodes:
                 self.beliefs[agent_id].scene.nodes.update(nodes)
 
-    def start_n2_situation(self, timeline, predicate, subject_name, object_name, isevent=False):
-        description = predicate + "(" + subject_name + "," + object_name + ")"
+    def start_predicate(self, timeline, predicate, subject_name, object_name=None, isevent=False):
+        if object_name is None:
+            description = predicate + "(" + subject_name + ")"
+        else:
+            description = predicate + "(" + subject_name + "," + object_name + ")"
         sit = Situation(desc=description)
         sit.starttime = time.time()
         if isevent:
             sit.endtime = sit.starttime
         self.current_situations_map[description] = sit
-        self.log_pub["situation_log"].publish("START " + description)
+        self.log_pub[predicate].publish("START " + description)
         timeline.update(sit)
         return sit.id
 
-    def end_n2_situation(self, timeline, predicate, subject_name, object_name):
-        description = predicate + "(" + subject_name + "," + object_name + ")"
+    def end_predicate(self, timeline, predicate, subject_name, object_name=None):
+        if object_name is None:
+            description = predicate + "(" + subject_name + ")"
+        else:
+            description = predicate + "(" + subject_name + "," + object_name + ")"
         try:
             sit = self.current_situations_map[description]
-            self.log_pub["situation_log"].publish("END " + description)
+            self.log_pub[predicate].publish("END " + description)
             timeline.end(sit)
         except Exception as e:
-            pass
-            #rospy.logwarn("[point_at_srv] Exception occurred : " + str(e))
+            rospy.logwarn("[perspective_filter] Exception occurred : " + str(e))
+
+    def isobject(self, scene, node):
+        isobject = False
+        if node != scene.rootnode:
+            if node.type == MESH:
+                if scene.nodes[node.parent].type != CAMERA:
+                    isobject = True
+        return isobject
 
     def publish_perspectives(self):
-        objects_seen = {}
-        agents_seen = {}
-        for agent_name, visible_nodes in self.visible_nodes.items():
-            objects_seen[agent_name] = []
-            agents_seen[agent_name] = []
+        header = Header()
+        header.frame_id = self.reference_frame
+        header.stamp = rospy.Time()
+        perspective_array_stamped = PerspectiveArrayStamped()
+        perspective_array_stamped.header = header
+        cpt = 0
+        rospy.logdebug("%s perspectives to send"%str(len(self.visible_nodes)))
+        for agent_id, visible_nodes in self.visible_nodes.items():
+            perspective_array_stamped.perspectives.append(Perspective())
+            perspective_array_stamped.perspectives[cpt].agent_id = str(agent_id)
+            perspective_array_stamped.perspectives[cpt].agent_name = str(self.source.scene.nodes[agent_id].name)
             for node in visible_nodes:
-                objects_seen[agent_name].append(node.name)
+                if self.isobject(self.source.scene, node):
+                    obj_msg = Object()
+                    obj_msg.id = node.id
+                    obj_msg.name = node.name
+                    t = translation_from_matrix(get_world_transform(self.source.scene, node))
+                    q = quaternion_from_matrix(get_world_transform(self.source.scene, node))
+                    pose = Pose()
+                    pose.position.x = t[0]
+                    pose.position.y = t[1]
+                    pose.position.z = t[2]
+                    pose.orientation.x = q[0]
+                    pose.orientation.y = q[1]
+                    pose.orientation.z = q[2]
+                    pose.orientation.w = q[3]
+                    pose_stamped = PoseStamped(header, pose)
+                    obj_msg.pose = pose_stamped
+                    perspective_array_stamped.perspectives[cpt].objects_seen.append(obj_msg)
 
-        perspectives = PerspectiveArray()
-        for name, objects in objects_seen.items():
-            perspective = Perspective()
-            perspective.agent_name = name
-            perspective.objects_seen = objects
-            perspective.agents_seen = agents_seen[name]
-            perspectives.perspectives.append(perspective)
+                if node.type == CAMERA:
+                    agent_msg = Agent()
+                    agent_msg.id = node.id
+                    agent_msg.name = node.name
+                    t = translation_from_matrix(get_world_transform(self.source.scene, node))
+                    q = quaternion_from_matrix(get_world_transform(self.source.scene, node))
+                    pose = Pose()
+                    pose.position.x = t[0]
+                    pose.position.y = t[1]
+                    pose.position.z = t[2]
+                    pose.orientation.x = q[0]
+                    pose.orientation.y = q[1]
+                    pose.orientation.z = q[2]
+                    pose.orientation.w = q[3]
+                    pose_stamped = PoseStamped(header, pose)
+                    agent_msg.head_gaze_pose = pose_stamped
+                    agent_bodies = []
+                    for child_id in node.children:
+                        node = self.source.scene.nodes[child_id]
+                        object_msg = Object()
+                        object_msg.id = node.id
+                        object_msg.name = node.name
+                        t = translation_from_matrix(get_world_transform(self.source.scene, node))
+                        q = quaternion_from_matrix(get_world_transform(self.source.scene, node))
+                        pose = Pose()
+                        pose.position.x = t[0]
+                        pose.position.y = t[1]
+                        pose.position.z = t[2]
+                        pose.orientation.x = q[0]
+                        pose.orientation.y = q[1]
+                        pose.orientation.z = q[2]
+                        pose.orientation.w = q[3]
+                        pose_stamped = PoseStamped(header, pose)
+                        object_msg.pose = pose_stamped
+                        agent_bodies.append(object_msg)
+                    agent_msg.agent_bodies = agent_bodies
+                    perspective_array_stamped.perspectives[cpt].agents_seen.append(agent_msg)
+            cpt += 1
 
-        self.ros_publishers["perspectives"].publish(perspectives)
+        self.ros_publishers["perspectives"].publish(perspective_array_stamped)
+        rospy.logdebug("[perspective_filter] %s perspectives published, %s should be" % (str(len(perspective_array_stamped.perspectives)), cpt))
 
     def compute_relations(self):
         """
@@ -202,33 +275,52 @@ class PerspectiveFilter(object):
         :return: The current relations map
         """
 
-        previously_visible_nodes = {}
         visible_nodes = {}
-        for camera_name, camera in self.cameras.items():
+
+        self.cameras = self.get_all_cameras()
+        rospy.logdebug(self.cameras)
+
+        if self.cameras.items():
             try:
-                if self.visible_nodes:
-                    previously_visible_nodes[camera.id] = self.visible_nodes[camera.id]
                 if self.visibility_monitor is None:
                     self.visibility_monitor = VisibilityMonitor(self.ctx, self.source)
-                    rospy.loginfo("[perspective_filter] Please activate the Pygame window to compute visibilities!")
+                    rospy.loginfo("[perspective_filter] Visibility monitor now running, please active the Pygame windows.")
+                visible_nodes = self.visibility_monitor.compute_all()
+                rospy.logdebug("[perspective_filter] %d perspectives computed " % len(visible_nodes))
+                #rospy.logdebug(visible_nodes)
+            except Exception as e:
+                rospy.logwarn("[perspective_filter] Exception occurred while computing relation : %s" % str(e))
+        if self.visibility_monitor:
+            self.visible_nodes = {} #visible_nodes
+            for camera_name, visibles_obj in visible_nodes.items():
+                camera_id = self.source.scene.nodebyname(camera_name)[0].id
+                self.visible_nodes[camera_id] = visibles_obj
+                for node in visibles_obj:
+                    if node.parent in self.cameras.keys():
+                        if self.source.scene.nodes[node.parent] not in visibles_obj:
+                            visibles_obj.append(self.source.scene.nodes[node.parent])
 
-                visible_nodes[camera.id] = self.visibility_monitor.from_camera(camera.name)
-            except Exception:
-                pass
-        self.visible_nodes = visible_nodes
+            for agent_id, nodes_seen in self.visible_nodes.items():
+                agent = self.source.scene.nodes[agent_id]
+                for node in nodes_seen:
+                    if agent_id in self.previously_visible_nodes:
+                        if node not in self.previously_visible_nodes[agent_id]:
+                            self.start_predicate(self.source.timeline, "isVisibleBy", node.name, object_name=agent.name)
+                    else:
+                        self.start_predicate(self.source.timeline, "isVisibleBy", node.name, object_name=agent.name)
 
-        for agent_id, nodes_seen in self.visible_nodes.items():
-            agent = self.source.scene.nodes[agent_id]
-            for node in nodes_seen:
-                if agent_id in previously_visible_nodes:
-                    if node not in previously_visible_nodes[agent_id]:
-                        self.start_n2_situation(self.source.timeline, "isVisibleBy", node.name, agent.name)
+            for agent_id, nodes_previously_seen in self.previously_visible_nodes.items():
+                agent = self.source.scene.nodes[agent_id]
+                for node in nodes_previously_seen:
+                    if agent_id in self.visible_nodes:
+                        if node not in self.visible_nodes[agent_id]:
+                            self.end_predicate(self.source.timeline, "isVisibleBy", node.name, object_name=agent.name)
+                    else:
+                        self.end_predicate(self.source.timeline, "isVisibleBy", node.name, object_name=agent.name)
 
-        for agent_id, nodes_previously_seen in previously_visible_nodes.items():
-            agent = self.source.scene.nodes[agent_id]
-            for node in nodes_previously_seen:
-                if node not in self.visible_nodes[agent_id]:
-                    self.end_n2_situation(self.source.timeline, "isVisibleBy", node.name, agent.name)
+            self.publish_perspectives()
+            self.previously_visible_nodes = self.visible_nodes
+
 
     def run(self):
         """
@@ -236,11 +328,13 @@ class PerspectiveFilter(object):
         :return:
         """
         rate = rospy.Rate(30)
+        #rospy.loginfo("[perspective_filter] Start running the ros node")
         while not rospy.is_shutdown():
             try:
                 self.compute_relations()
-                #self.publish_perspectives()
-                self.filter()
+                if self.visibility_monitor:
+                    self.filter()
+                    #self.publish_perspectives()
                 rate.sleep()
             except KeyboardInterrupt:
                 break
@@ -253,8 +347,10 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Monitor visibilities & manage the beliefs worlds")
     parser.add_argument("source_world", help="Underworlds source world to monitor")
+    parser.add_argument("--reference_frame", default="map", help="The reference frame of the system")
     args = parser.parse_args()
-
-    rospy.init_node("perspective_filter", anonymous=True)
+    rospy.loginfo("[perspective_filter] Initialize ros node")
+    rospy.init_node("perspective_filter", anonymous=True)#, log_level=rospy.DEBUG)
+    rospy.loginfo("[perspective_filter] Connecting to Underworlds server...")
     with underworlds.Context("Perspective Filter") as ctx:
-        PerspectiveFilter(ctx, args.source_world).run()
+        PerspectiveFilter(ctx, args.source_world, args.reference_frame).run()
